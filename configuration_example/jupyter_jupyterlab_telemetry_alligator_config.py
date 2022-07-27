3f8362a620d6a0d52d1a49ca072f45efae6994a4
null <adpatter@umich.edu>
2022-07-27 16:45:33 -0400

    debug

--- configuration_example/jupyter_jupyterlab_telemetry_alligator_config.py
+++ configuration_example/jupyter_jupyterlab_telemetry_alligator_config.py
@@ -1,6 +1,5 @@
 #  This file should be saved into one of the config directories provided by `jupyter lab --path`.
 
-#  The url of the S3 bucket, which will comprise the first component of the bucket_url.
 c.JupyterLabTelemetryAlligatorApp.telemetry_path = "/logs/alligator/telemetry"
 
 #  Telemetry can be turned on by either setting telemetry to True or by touching the .telemetry file in the Lab home directory.
