f22b664db13275d194214eb8bcdd34af28f5b455
null <adpatter@umich.edu>
2022-07-28 14:07:40 -0400

    deploy

--- configuration_example/jupyter_jupyterlab_telemetry_alligator_config.py
+++ configuration_example/jupyter_jupyterlab_telemetry_alligator_config.py
@@ -1,6 +1,6 @@
 #  This file should be saved into one of the config directories provided by `jupyter lab --path`.
 
-c.JupyterLabTelemetryAlligatorApp.efs_path = "/data/alligator"
+c.JupyterLabTelemetryAlligatorApp.efs_path = "/.data/alligator"
 
 #  Telemetry can be turned on by either setting telemetry to True or by touching the .telemetry file in the Lab home directory.
 c.JupyterLabTelemetryAlligatorApp.telemetry = True
